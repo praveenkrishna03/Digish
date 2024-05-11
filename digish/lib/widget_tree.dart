d999459c365bb5a4abccc415631d8f723bf3dc13
Praveen Krishna <gpkgpk2003@gmail.com>
2024-05-11 11:33:03 +0530

    modified:   digish/lib/main.dart 	new file:   digish/lib/widget_tree.dart

---
+++ digish/lib/widget_tree.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class WidgetTree extends StatefulWidget {
+  @override
+  State<WidgetTree> createState() => _WidgetTreeState();
+}
+
+class _WidgetTreState extends State<WidgetTree>{
+  @override
+  Widget build(BuildContext context){
+    return StreamBuilder(stream: stream, builder: builder)
+  }
+
+}
